41628fed09db433b8a473e99eb95a7b6098367b9
Micah Parks <micahleviparks@gmail.com>
2020-02-24 19:30:06 -0500

    Missing example.

---
+++ examples/statsSite.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"gitlab.com/MicahParks/wigole"
+	"gitlab.com/MicahParks/wigole/stats/site"
+	"gitlab.com/MicahParks/wigole/user"
+)
+
+type creds struct {
+	Password string
+	Username string
+}
+
+func main() {
+	// Query for site statistics. Print out the number of WPA2 networks.
+	cred := creds{}
+	c, err := ioutil.ReadFile("creds.json")
+	if err != nil {
+		println("Create a properly formatted 'creds.json' file in the working directory.")
+		return
+	}
+	if err = json.Unmarshal(c, &cred); err != nil {
+		println("JSON failure for 'creds.json' file in the working directory.")
+		return
+	}
+	u := user.New(cred.Password, cred.Username)
+	s := site.New()
+	resp, err := s.Do(u)
+	if err != nil {
+		if err == wigole.ErrTooMany {
+			println("Too many queries of that type for today.")
+			return
+		}
+		if err == wigole.ErrAuth {
+			println("Failed to authenticate with creds.json.")
+			return
+		}
+		panic(err)
+	}
+	println(resp.Netwpa2)
+}
