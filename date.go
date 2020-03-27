920b036a0fa2774fae14371ce915e6a78278e25b
Micah Parks <micahleviparks@gmail.com>
2020-03-27 11:50:32 -0400

    More docs.

--- date.go
+++ date.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// TimeString converts the given time.Time to the string format that WiGLE API wants.
 func TimeString(t time.Time) string {
 	// yyyyMMdd[hhmm[ss]]
 	return fmt.Sprintf("%d%02d%02d%02d%02d%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
